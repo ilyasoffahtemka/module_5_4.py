0096c7f713878453b36a6a83eb908a8e3aa710b3
ilyasoffahtemka <akhtem.ilyasov@gmail.com>
2024-10-23 21:45:36 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,33 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+
+        obj = super().__new__(cls)
+        cls.houses_history.append(args[0])
+        return obj
+
+    def __init__(self, name, floors):
+
+        self.name = name
+        self.floors = floors
+
+    def __del__(self):
+
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+
+del h2
+del h3
+
+print(House.houses_history)
